bc21b108effc9b873891ed2fb74fbb499a1031f4
Gareth Noyce <garethnoyce@triple-eh.net>
2016-03-17 10:44:22 +0200

    German was missing Game Over

--- generate_loc.py
+++ generate_loc.py
@@ -52,13 +52,88 @@ def main(argv):
 
     outputFile.write ('\t};\n\n')
 
+   
+   
     print('English strings ...')
     outputFile.write('\tpublic static string[] LOC_EnglishStrings = {\n' )
 
     for i in range (len(sheet1)-1):
         outputFile.write('\t\t\"'+ sheet1[i][1] + '\",\n')
 
-    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][1] + '\"\n\t};\n}')
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][1] + '\"\n\t};\n')
+
+    
+    
+    print('French Strings...')
+    outputFile.write('\tpublic static string[] LOC_FrenchStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][2] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][2] + '\"\n\t};\n')
+
+
+    print('German strings...')
+    outputFile.write('\tpublic static string[] LOC_GermanStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][3] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][3] + '\"\n\t};\n')
+
+
+    print('Spanish strings...')
+    outputFile.write('\tpublic static string[] LOC_SpanishStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][4] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][4] + '\"\n\t};\n')
+
+
+    print('Italian strings...')
+    outputFile.write('\tpublic static string[] LOC_ItalianStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][5] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][5] + '\"\n\t};\n')
+
+
+    print('Russian strings...')
+    outputFile.write('\tpublic static string[] LOC_RussianStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][6] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][6] + '\"\n\t};\n')
+
+    print('Brazilian strings')
+    outputFile.write('\tpublic static string[] LOC_BrazilianStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][7] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][7] + '\"\n\t};\n')
+
+
+    print('Japanese strings')
+    outputFile.write('\tpublic static string[] LOC_JapaneseStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][8] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][8] + '\"\n\t};\n')
+
+
+    print('Finnish strings...')
+    outputFile.write('\tpublic static string[] LOC_FinnishStrings = {\n' )
+
+    for i in range (len(sheet1)-1):
+        outputFile.write('\t\t\"'+ sheet1[i][9] + '\",\n')
+
+    outputFile.write('\t\"'+ sheet1[len(sheet1)-1][9] + '\"\n\t};\n}')
+
 
 
     outputFile.close()
