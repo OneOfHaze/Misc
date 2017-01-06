74aa6728c918f0fa9a77dd7bd8da0f23e9a43f17
Gareth Noyce <garethnoyce@triple-eh.net>
2017-01-06 16:37:50 +0200

    Delete UploadTDI.sh

--- Blogs/UploadTDI.sh
+++
@@ -1,7 +0,0 @@
-cd ./tdi.online_jekyll
-jekyll build
-git add .
-git commit -m "New post"
-git push origin
-cd ..
-rsync -v -rz --delete --checksum ./tdi.online_jekyll/_site/ korruptor@tdi.online:/var/www/www.tdi.online/
